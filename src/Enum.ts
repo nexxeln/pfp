68a92031af7a6327fec5b954afb2fdc7a320599c
Shoubhit Dash <shoubhit2005@gmail.com>
2023-01-31 16:29:03 +0530

    +(enum): add any

--- src/Enum.ts
+++ src/Enum.ts
@@ -1,8 +1,14 @@
 const all = <T>(enumerable: T[], fun?: (element: T) => unknown): boolean =>
   fun
-    ? enumerable.every((element) => fun(element))
+    ? enumerable.every((element) => Boolean(fun(element)))
     : enumerable.every((element) => Boolean(element));
+
+const any = <T>(enumerable: T[], fun?: (element: T) => unknown): boolean =>
+  fun
+    ? enumerable.some((element) => Boolean(fun(element)))
+    : enumerable.some((element) => Boolean(element));
 
 export const Enum = {
   all,
+  any,
 };
--- src/Enum_test.ts
+++ src/Enum_test.ts
@@ -49,3 +49,49 @@ Deno.test("Enum.all()", () => {
     "returns false if fun(element) is falsy for any element in enumerable"
   );
 });
+
+Deno.test("Enum.any()", () => {
+  assertEquals(
+    Enum.any([0, 2, 3]),
+    true,
+    "returns true if at least one element in enumerable is truthy"
+  );
+  assertEquals(
+    Enum.any(["", "b", ""]),
+    true,
+    "returns true if at least one element in enumerable is truthy"
+  );
+
+  assertEquals(
+    Enum.any([0, 0, 0]),
+    false,
+    "returns false if all elements in enumerable are falsy"
+  );
+  assertEquals(
+    Enum.any(["", "", ""]),
+    false,
+    "returns false if all elements in enumerable are falsy"
+  );
+
+  assertEquals(
+    Enum.any([-2, -2, -3], (x) => x + 2),
+    true,
+    "returns true if fun(element) is truthy for at least one element in enumerable"
+  );
+  assertEquals(
+    Enum.any(["a", "b", "c"], (x) => x.replace("a", "")),
+    true,
+    "returns true if fun(element) is truthy for at least one element in enumerable"
+  );
+
+  assertEquals(
+    Enum.any([1, 2, 3], (x) => x < 1),
+    false,
+    "returns false if fun(element) is falsy for all elements in enumerable"
+  );
+  assertEquals(
+    Enum.any(["a", "b", "c"], (x) => x.length > 1),
+    false,
+    "returns false if fun(element) is falsy for all elements in enumerable"
+  );
+});
